8b8bd53d92ff8e26fee453f7f83837fec6a08797
yasha32 <yakov.zverev32@gmail.com>
2021-12-21 17:09:19 +0300

    fixed sockets

--- public/index.php
+++ public/index.php
@@ -1,36 +1,9 @@
 <?php
 
-use Workerman\Worker;
+use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$ws_worker = new Worker("websocket://0.0.0.0:2446");
-
-// 4 processes
-$ws_worker->count = 4;
-
-// Emitted when new connection come
-$ws_worker->onConnect = function($connection)
-{
-    // Emitted when websocket handshake done
-    $connection->onWebSocketConnect = function($connection)
-    {
-        echo "New connection\n";
-    };
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-// Emitted when data is received
-$ws_worker->onMessage = function($connection, $data)
-{
-    // Send hello $data
-    $connection->send('hello ' . $data);
-};
-
-// Emitted when connection closed
-$ws_worker->onClose = function($connection)
-{
-    echo "Connection closed";
-};
-
-// Run worker
-Worker::runAll();
