1d80946609bcd66941cfeac9dca248170ce5d2d5
yasha32 <yakov.zverev32@gmail.com>
2021-12-21 16:38:26 +0300

    added workerman sockets

--- public/index.php
+++ public/index.php
@@ -1,9 +1,36 @@
 <?php
 
-use App\Kernel;
+use Workerman\Worker;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+$ws_worker = new Worker("websocket://0.0.0.0:2446");
+
+// 4 processes
+$ws_worker->count = 4;
+
+// Emitted when new connection come
+$ws_worker->onConnect = function($connection)
+{
+    // Emitted when websocket handshake done
+    $connection->onWebSocketConnect = function($connection)
+    {
+        echo "New connection\n";
+    };
 };
+
+// Emitted when data is received
+$ws_worker->onMessage = function($connection, $data)
+{
+    // Send hello $data
+    $connection->send('hello ' . $data);
+};
+
+// Emitted when connection closed
+$ws_worker->onClose = function($connection)
+{
+    echo "Connection closed";
+};
+
+// Run worker
+Worker::runAll();
